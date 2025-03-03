138e1c1f63e6e2b1be10247411176b284943e3b7
d3thfox <vcvit2004@gmail.com>
2025-03-03 14:23:32 +0600

    написал файлы под докер

--- afisha/settings.py
+++ afisha/settings.py
@@ -15,7 +15,7 @@ import os
 from dotenv import read_dotenv 
 
 read_dotenv()
- 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
